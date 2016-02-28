43545052481516c3245afb72329ac3f4c3d662f5
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2016-02-28 12:23:14 +1030

    completed GrowSquarePrinter class

--- src/chapter2/GrowSquarePrinter.java
+++ src/chapter2/GrowSquarePrinter.java
@@ -1,10 +1,21 @@
 package chapter2;
 
+import java.awt.Rectangle;
+
 public class GrowSquarePrinter {
 	
 	public static void main (String[] args) {
 		
+		Rectangle square = new Rectangle(100,100,50,50);
+		System.out.println("Original: " + square);
+		
+		int growBy = (int)(square.getWidth() / 2);
+		square.grow(growBy, growBy);
+		square.translate(growBy, growBy);
+		
+		System.out.println("Transformed: " + square);
 		
 	}
 
 }
+
